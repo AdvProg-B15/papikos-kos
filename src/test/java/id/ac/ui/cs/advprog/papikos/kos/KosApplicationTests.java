6ae8c2339e2b2a5da9ffe6b63765ba89cd3bd807
Carleano Ravelza Wongso <carleanoravel@gmail.com>
2025-04-11 16:51:20 +0700

    initial commit

---
+++ src/test/java/id/ac/ui/cs/advprog/papikos/kos/KosApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.papikos.kos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
